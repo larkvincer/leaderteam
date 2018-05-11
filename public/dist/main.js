0b5c9e83ecd75a25de0c5e2cbd4c66760447daaf
Pavlo Kuziv <larkvincer@gmail.com>
2018-05-11 17:59:09 +0300

    Manage static files.

---
+++ public/dist/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	const loginForm = document.querySelector('.form--type-login');
+	loginForm.addEventListener('submit', function(event) {
+		event.preventDefault();
+	});
+
+});
