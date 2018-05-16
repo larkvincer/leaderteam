ac9d4329f21142e0920d54646a4ddcf17275ad62
Pavlo Kuziv <larkvincer@gmail.com>
2018-05-16 15:40:42 +0300

    Make fetch on login.

--- public/dist/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	const loginForm = document.querySelector('.form--type-login');
-	loginForm.addEventListener('submit', function(event) {
-		event.preventDefault();
-	});
-
-});
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-
 	const loginForm = document.querySelector('.form--type-login');
+
 	loginForm.addEventListener('submit', function(event) {
+		fetch(`${HOST}`, {
+			method: 'POST',
+		}).then(function(response) {
+			console.log(response);
+			if (response.status === 200) {
+				return response.json();
+			}
+		}).then(function(data) {
+			window.location.pathname = data.redirect;
+		});
 		event.preventDefault();
 	});
-
 });
