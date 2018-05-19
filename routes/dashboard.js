413733d2532f92723834f13e714ce90ebbf26fab
Pavlo Kuziv <larkvincer@gmail.com>
2018-05-20 00:10:35 +0300

    Complete dashboard template.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-	if (req.session.counter) {
-		req.session.counter++;
-		res.send('Counter has been already created. Counter ' + req.session.counter);
-	} else {
-		req.session.counter = 1;
-	}
+	// if (req.session.counter) {
+	// 	req.session.counter++;
+	// 	res.send('Counter has been already created. Counter ' + req.session.counter);
+	// } else {
+	// 	req.session.counter = 1;
+	// }
 	res.render('dashboard');
 });
 
