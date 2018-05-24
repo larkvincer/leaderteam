1148e93de3b9c12c7fd75c3ae1432f6fd90349c8
Pavlo Kuziv <larkvincer@gmail.com>
2018-05-24 15:28:29 +0300

    Add browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
+'use strict';
+
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const minifyCSS = require('gulp-csso');
 const minifyJS = require('gulp-uglify');
+const browserify = require('browserify');
+const stream = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const sourcemaps = require('gulp-sourcemaps');
+const log = require('gulplog');
+
 
 gulp.task('css-production', function() {
 	return gulp.src('./public/stylesheets/*.css')
@@ -16,24 +24,28 @@ gulp.task('css-development', function() {
 		.pipe(gulp.dest('./public/dist/'));
 });
 
-gulp.task('js-production', function() {
-	return gulp.src('./public/javascripts/*.js')
-		.pipe(concat('main.js'))
-		.pipe(minifyJS('main-min.js'))
+gulp.task('javascript', function() {
+	// set up the browserify instance on a task basis
+	let b = browserify({
+		entries: './public/javascripts/index.js',
+		debug: true,
+	});
+
+	return b.bundle()
+		.pipe(stream('main.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({loadMaps: true}))
+	// Add transformation tasks to the pipeline here.
+		.pipe(minifyJS())
+		.on('error', log.error)
+		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./public/dist/'));
 });
 
-gulp.task('js-development', function() {
-	return gulp.src('./public/javascripts/*.js')
-		.pipe(concat('main.js'))
-		.pipe(gulp.dest('./public/dist'));
-});
-
 gulp.task('build', ['js-production', 'css-production']);
-gulp.task('dev-build', ['js-development', 'css-development']);
 
 gulp.task('js-watch', function() {
-	return gulp.watch('./public/javascripts/*.js', ['js-development']);
+	return gulp.watch('./public/javascripts/*.js', ['javascript']);
 });
 gulp.task('css-watch', function() {
 	return gulp.watch('./public/stylesheets/*.css', ['css-development']);
