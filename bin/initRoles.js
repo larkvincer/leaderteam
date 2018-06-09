afc2717e8541403b1d36e8813ffd1e745556383f
Pavlo Kuziv <larkvincer@gmail.com>
2018-06-10 00:58:34 +0300

    Add more permissions.

--- bin/initRoles.js
+++ bin/initRoles.js
@@ -12,21 +12,49 @@ console.log('START CREATION OF ROLES.');
 			action.CREATE_MANAGER,
 			action.EDIT_MANAGER,
 			action.LIST_MANAGERS,
+
 			action.CREATE_SUPERVISER,
 			action.EDIT_SUPERVISER,
 			action.LIST_SUPERVISERS,
+
+			action.CREATE_MERCHANDISER,
+			action.LIST_MERCHANDISERS,
+			action.EDIT_MERCHANDISER,
+
 			action.CREATE_CLIENT,
 			action.EDIT_CLIENT,
 			action.LIST_CLIENTS,
+
 			action.CREATE_TASK,
 			action.LIST_TASKS,
+			action.EDIT_TASK,
+
+			action.CREATE_MARKETNET,
+			action.LIST_MARKETNETS,
+			action.EDIT_MARKETNET,
+
+			action.CREATE_MARKET,
 			action.LIST_MARKETS,
+			action.LIST_MARKETS,
+
+			action.CREATE_COMMENT,
+			action.EDIT_COMMENT,
 		],
 	});
 	const Superviser = new Role({
 		name: roles.SUPERVISER,
 		actions: [
 			action.CREATE_TASK,
+			action.LIST_TASKS,
+			action.EDIT_TASK,
+			action.COMPLETE_TASK,
+
+			action.LIST_SUBORDINATED_MERCH,
+
+			action.LIST_ATTACHED_CLIENTS,
+
+			action.CREATE_COMMENT,
+			action.EDIT_COMMENT,
 		],
 	});
 	const Merchandiser = new Role({
@@ -34,12 +62,18 @@ console.log('START CREATION OF ROLES.');
 		actions: [
 			action.LIST_TASKS,
 			action.COMPLETE_TASK,
+
+			action.CREATE_COMMENT,
+			action.EDIT_COMMENT,
 		],
 	});
 	const Client = new Role({
 		name: roles.CLIENT,
 		actions: [
 			action.LIST_TASKS,
+
+			action.CREATE_COMMENT,
+			action.EDIT_COMMENT,
 		],
 	});
 
--- models/constants/actions.js
+++ models/constants/actions.js
@@ -10,14 +10,17 @@ module.exports = Object.freeze({
 	CREATE_MERCHANDISER: 'create merchandiser',
 	EDIT_MERCHANDISER: 'edit merchandiser',
 	LIST_MERCHANDISERS: 'list merchandisers',
+	LIST_SUBORDINATED_MERCH: 'list subordinated merchandisers',
 
 	CREATE_CLIENT: 'create client',
 	EDIT_CLIENT: 'edit client',
 	LIST_CLIENTS: 'list clients',
+	LIST_ATTACHED_CLIENTS: 'list attached clients',
 
 	CREATE_TASK: 'create task',
 	COMPLETE_TASK: 'complete task',
 	LIST_TASKS: 'list tasks',
+	EDIT_TASK: 'edit task',
 
 	CREATE_MARKETNET: 'create market network',
 	EDIT_MARKETNET: 'edit market network',
@@ -26,4 +29,7 @@ module.exports = Object.freeze({
 	CREATE_MARKET: 'create market',
 	EDIT_MARKET: 'edit market',
 	LIST_MARKETS: 'list markets',
+
+	CREATE_COMMENT: 'create comment',
+	EDIT_COMMENT: 'edit comment',
 });
